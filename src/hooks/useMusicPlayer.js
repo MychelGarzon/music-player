f6376d9d3b4762a1d69b68f4c7875308fb7f55af
Mychel Garzon <mtjomy7@gmail.com>
2024-03-01 11:04:16 +0200

    Refactor useMusicPlayer hook to add playTrack and playPreviousTrack functions

--- src/hooks/useMusicPlayer.js
+++ src/hooks/useMusicPlayer.js
@@ -2,9 +2,37 @@ import { useContext } from "react"
 import { MusicContext } from "../contexts/MusicContext";
 
 const useMusicPlayer = () => {
+
     const [state, setState] = useContext(MusicContext);
 
+    const tooglePlay = () => {
+        if (state.isPlaying) {
+            state.audioPlayer.pause();
+            setState({ ...state, isPlaying: false });
+        } else {
+            state.audioPlayer.play();
+            setState({ ...state, isPlaying: true });
+        }
+    }
 
-}
+    const playTrack = (index) => {
+        if (index === state.currentTrackIndex) {
+            tooglePlay();
+        } else {
+            state.audioPlayer.pause();
+            state.audioPlayer = new Audio(state.tracks[index].file);
+            state.audioPlayer.play();
+            setState({ ...state, currentTrackIndex: index, isPlaying: true });
+        }
 
-export { useMusicPlayer }
+    }
+    const playPreviousTrack = () => {
+        let newIndex = null;
+        state.currentTrackIndex === 0
+            ? (newIndex = state.tracks.length - 1)
+            : (newIndex = state.currentTrackIndex - 1)
+        playTrack(newIndex);
+    }
+
+};
+export { useMusicPlayer };
