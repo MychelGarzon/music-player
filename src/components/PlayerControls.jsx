1ac21f91e5784b82e3fc8f8ea9f1f3a649b5d2de
Mychel Garzon <mtjomy7@gmail.com>
2024-03-01 10:45:51 +0200

    Add music player controls to PlayerControls component

--- src/components/PlayerControls.jsx
+++ src/components/PlayerControls.jsx
@@ -1,6 +1,28 @@
 'use client';
 
-const PlayerControls = () => {
+import { PlayFill, SkipEndFill, SkipStartFill } from "react-bootstrap-icons";
 
+const PlayerControls = () => {
+    //TODO: ADD useMuscPlayer hook
+
+    return (
+        <div>
+            <div>
+                <p>Here I will be current track name</p>
+            </div>
+            <button>
+                <SkipStartFill />
+            </button>
+
+            <button>
+                <PlayFill />
+            </button>
+            <button>
+                <SkipEndFill />
+            </button>
+        </div>
+    )
 
 };
+
+export default PlayerControls;
